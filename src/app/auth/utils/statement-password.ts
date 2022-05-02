cec9ad2262eec7b4239870a1c9595e2198e6c370
Cristopher <cristopher.emodesto@gmail.com>
2022-05-01 23:41:45 -0500

    Updated password, statement validators

---
+++ src/app/auth/utils/statement-password.ts
@@ -0,0 +1,10 @@
+import { FormControl, FormGroupDirective, NgForm } from "@angular/forms";
+
+export class MyErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control?.invalid && control?.parent?.dirty);
+    const invalidParent = !!(control?.parent?.invalid && control?.parent?.dirty);
+
+    return invalidCtrl || invalidParent;
+  }
+}
